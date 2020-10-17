fb8cdc03d11e8d644306b4c99a0f1ef6914ab0dc
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2020-10-16 23:30:30 -0300

    NLW3 day #4 complete

---
+++ src/database/fakedata.js
@@ -0,0 +1,30 @@
+module.exports = [
+    {
+        id: 1,
+        lat: '-27.222633',
+        lng: '-49.6455874',
+        name: 'Lar das meninas',
+        description: 'Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.',
+        images: [
+            'https://images.unsplash.com/photo-1535726858289-9ffe2dff6f52?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60',
+            'https://images.unsplash.com/photo-1599140781147-e9a53c8cdd03?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+        ],
+        instructions: 'Venha quando se sentir a vontade e traga muito amor e paciência para dar.',
+        opening_hours: 'Horário de visitas Das 8h até 18h.',
+        open_on_weekends: '1'
+    },
+    {
+        id: 2,
+        lat: '-27.222633',
+        lng: '-49.6555874',
+        name: 'Lar dos meninos',
+        description: 'Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.',
+        images: [
+            'https://images.unsplash.com/photo-1535726858289-9ffe2dff6f52?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60',
+            'https://images.unsplash.com/photo-1599140781147-e9a53c8cdd03?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+        ],
+        instructions: 'Venha quando se sentir a vontade e traga muito amor e paciência para dar.',
+        opening_hours: 'Horário de visitas Das 8h até 18h.',
+        open_on_weekends: '0'
+    }
+]
